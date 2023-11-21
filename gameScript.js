912337f00a82129537ee6f3747f4cf3982c57d0a
Jocelyn Lahaise <wlahaise@albany.edu>
2023-11-20 22:19:44 -0500

    keyboard, backspace and letters work.

--- gameScript.js
+++ gameScript.js
@@ -1,24 +1,88 @@
 guesses = 0;
-letters = 0;
+letters = -1;
+wordOfTheDay = "HOME";
 
-function keyPress(input) {
-    console.log(input)
+document.addEventListener("keyup", (event) =>
+{
+    if(guesses >= 4)
+    {
+        console.log("No guesses left.")
+        return
+    }
+
+    let keyPressValue = String(event.key)
+
+    switch(keyPressValue)
+    {
+        case "Backspace":
+            backspacePress()
+            break
+        case "Enter":
+            enterPress()
+            break
+        case keyPressValue:
+            let charValue = keyPressValue.charAt(0)
+            if(charValue.toUpperCase() !== charValue)
+            {
+                keyPress(keyPressValue.toUpperCase())
+            }
+            break
+        default:
+            return
+    }
+})
+
+function keyPress(input)
+{
+    if(letters >= 3)
+    {
+        // If we have reached the maximum amount of letters, return since we don't want the letter to change.
+        return;
+    }
+    letters++;
     var guessTable = document.getElementById("guess-table")
     var tile = guessTable.rows.item(guesses).getElementsByClassName("tile").item(letters)
-    console.log(tile);
     tile.innerHTML = input;
-    letters++;
 }
 
-function enterPress() {
+
+
+function enterPress()
+{ // seems to add last used letter sometimes?
+    if(letters < 3)
+    {
+        console.log("Guessed word is less than 4 letters.");
+        return;
+    }
+
+    let wordGuessed = ""
+
+    for(let i = 0; i < 4; i++) {
+        var guessTable = document.getElementById("guess-table")
+        var tile = guessTable.rows.item(guesses).getElementsByClassName("tile").item(i)
+        wordGuessed += tile.innerHTML;
+    }
+    console.log("hii this is the worddd " + wordGuessed)
+
+    tile.style.backgroundColor = "green";
+
     for(let row of guessTable.rows) {
-        for(let cell of row.cells) {
+        for (let cell of row.cells) {
             //cell.getElementsByClassName("tile").item(0).innerHTML = input;
 
         }
     }
 }
 
-function backspacePress() {
-
+function backspacePress()
+{
+    if(letters === -1)
+    {
+        console.log("Returned since there were no letters.")
+        return;
+    }
+    var guessTable = document.getElementById("guess-table")
+    var tile = guessTable.rows.item(guesses).getElementsByClassName("tile").item(letters)
+    tile.innerHTML = ""
+    letters--;
 }
